f3d79a4fee8b6f611f11f5982329a07ded8f37a8
Kent Slaney <kent@slaney.org>
2025-09-20 23:02:51 -0700

    normalize sobel operator

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import pathlib
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.ndimage import sobel
-from functools import partial
+from functools import partial, cached_property
+import sympy as sy
 
 local = pathlib.Path(__file__).parents[0]
 mlmodel = None
@@ -41,7 +42,7 @@ im4 = fs(local / "IMG_0004.HEIC", local / "out4.npy")
 im5 = fs(local / "IMG_0005.HEIC", local / "out5.npy")
 
 def grad(arr):
-    return sobel(arr, 0), sobel(arr, 1)
+    return sobel(arr, 0) / 8, sobel(arr, 1) / 8
 
 def hessian(d0, d1):
     return list(map(grad, [d0, d1]))
@@ -124,12 +125,15 @@ def slide0(arr):
     d0, d1 = grad(arr)
     return slide(partial(normals, d0, d1), valmin=0, valmax=8, valinit=0)
 
-def slide1(arr):
+def adjust(arr):
     d0, d1 = grad(arr)
     sq = hessian(d0, d1)
     para, perp = eigenvalues2x2(sq)
     coef = np.divide(para, perp ** 3, out=np.ones_like(arr), where=perp != 0)
-    s0, s1 = d0 * coef, d1 * coef
+    return d0 * coef, d1 * coef
+
+def slide1(arr):
+    s0, s1 = adjust(arr)
     return slide(partial(casts, arr, s0, s1), valmin=0, valmax=1, valinit=0.1)
 
 def density(arr, samples=100, scale=4):
@@ -147,18 +151,68 @@ def blur(arr, sigma=3):
 def ndmax(arr):
     return np.unravel_index(arr.argmax(), arr.shape)
 
-def fake_sphere(r=np.min(target) // 5):
-    out = np.zeros(target)
-    c1, c0 = np.meshgrid(*map(np.arange, target[::-1]))
+class Sphere:
     t0, t1 = target // 2
-    d2 = (t0 - c0) ** 2 + (t1 - c1) ** 2
-    out[d2 < r ** 2] = np.sqrt(r ** 2 - d2[d2 < r ** 2])
-    out *= 0.8 / np.max(out)
-    out[d2 < r ** 2] += 0.1
-    return out
+    offset = sy.Rational(1, 10)
+    def __init__(self, r=np.min(target) // 5):
+        self.r = r
+        self.a, self.b = self.dim = sy.symbols('a b')
+
+    @cached_property
+    def rescale(self):
+        return sy.Rational(4, 5 * self.r)
+
+    @cached_property
+    def im(self):
+        out = np.zeros(target)
+        c1, c0 = np.meshgrid(*map(np.arange, target[::-1]))
+        d2 = (self.t0 - c0) ** 2 + (self.t1 - c1) ** 2
+        out[d2 < self.r ** 2] = np.sqrt(self.r ** 2 - d2[d2 < self.r ** 2])
+        out *= float(self.rescale)
+        out[d2 < self.r ** 2] += float(self.offset)
+        return out
+
+    def sym(self):
+        return self.offset + self.rescale * sy.sqrt(
+                self.r ** 2 - (self.t0 - self.a) ** 2 - (self.t1 - self.b) ** 2)
+
+    def grad(self):
+        expr = self.sym()
+        return sy.Matrix([[sy.diff(expr, self.a)], [sy.diff(expr, self.b)]])
+
+    def hessian(self):
+        expr = self.grad()
+        return sy.Matrix.hstack(sy.diff(expr, self.a), sy.diff(expr, self.b))
+
+    def eigenvalues(self):
+        expr = self.hessian()
+        tr = expr.trace()
+        center = tr / 2
+        radius = sy.sqrt(tr ** 2 / 4 - expr.det())
+        return sy.Matrix([[center - radius], [center + radius]])
+
+    def coef(self):
+        para, perp = self.eigenvalues()
+        return para / perp ** 3
+
+    def adjusted(self):
+        return self.coef() * self.grad()
+
+    def at(self, *args):
+        return list(zip(self.dim, args))
+
+    def subs(self, a, b):
+        return self.adjusted().subs(self.at(a, b))
+
+    @cached_property
+    def approx(self):
+        return adjust(self.im)
+
+    def cmp(self, a, b):
+        return [i[a, b] for i in self.approx], self.subs(a, b).evalf().tolist()
 
 def tmp0():
-    slide1(fake_sphere())
+    slide1(Sphere().im)
 
 # https://www.desmos.com/c/h0pkuzhfzh
 # https://www.desmos.com/3d/9ijh5b7ok9
@@ -170,7 +224,24 @@ def tmp1(im):
         plt.plot(i)
     plt.show()
 
-tmp0()
+def tmp2(delta0=-10, delta1=-10):
+    mock = Sphere()
+    ex = (mock.t0 + delta0, mock.t1 + delta1)
+    print("outputs", mock.cmp(*ex))
+    print("hessians")
+    print([j[*ex] for i in hessian(*grad(mock.im)) for j in i])
+    print(mock.hessian().subs(mock.at(*ex)).evalf())
+    print("grad")
+    print([j[*ex] for j in grad(mock.im)])
+    print(mock.grad().subs(mock.at(*ex)).evalf())
+    print("value")
+    print(mock.im[*ex])
+    print(mock.sym().subs(mock.at(*ex)).evalf())
+    print("3x3 numerical")
+    for i in [-1, 0, 1]:
+        for j in [-1, 0, 1]:
+            print(mock.im[ex[0] + i, ex[1] + j], end=" ")
+    print()
 
-# if __name__ == "__main__":
-#     slide(im4)
+if __name__ == "__main__":
+    tmp0()
