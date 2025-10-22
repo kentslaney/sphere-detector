9b6d14682b57c91e997e2bea79a7d0f2bde0e4a7
Kent Slaney <kent@slaney.org>
2025-10-22 10:32:32 -0700

    extra classification examples

---
+++ ds21.py
@@ -0,0 +1,15 @@
+balls = [
+    (429, 'n02799071', 'baseball'),
+    (430, 'n02802426', 'basketball'),
+    (522, 'n03134739', 'croquet_ball'),
+    (574, 'n03445777', 'golf_ball'),
+    (722, 'n03942813', 'ping-pong_ball'),
+    (805, 'n04254680', 'soccer_ball'),
+    (852, 'n04409515', 'tennis_ball'),
+    (890, 'n04540053', 'volleyball')
+]
+
+# by using this link you are subject to the ImageNet terms and conditions
+# please formally request access via image-net.org before redistribution
+# open source is only sustainable with integrity
+base_url = "https://image-net.org/data/winter21_whole/"
