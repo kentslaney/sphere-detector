6f5e65f5eacbddde7a1ad4a697b98bb766eded6a
Kent Slaney <kent@slaney.org>
2025-10-23 10:58:41 -0700

    add classification data

--- ds21.py
+++ ds21.py
@@ -17,26 +17,27 @@ finally:
 base_url = "https://image-net.org/data/winter21_whole"
 filename = lambda synset: f"{synset}.tar"
 synset_url = lambda synset: f"{base_url}/{synset}.tar"
-sources = [
-        { "url": synset_url(synset), "tar": filename(synset), "dir": readable }
-        for _, synset, readable in balls]
+sources = lambda synset, readable: {
+        "url": synset_url(synset), "tar": filename(synset), "dir": readable }
 
 data_dir = local / "assets" / "data"
-download_dir = data_dir / "downloads"
+download_dir = local / "assets" / "downloads"
 
 data_dir.mkdir(parents=True, exist_ok=True)
 download_dir.mkdir(parents=True, exist_ok=True)
 
-for info in sources:
-    dest = data_dir / info["dir"]
-    if dest.is_dir():
-        continue
-    tar = download_dir / info["tar"]
-    if not tar.is_file():
-        print("downloading", info["url"])
-        urllib.request.urlretrieve(info["url"], tar)
-    with tarfile.open(tar) as fp:
-        fp.extractall(dest, filter="data")
+def dl21(sources):
+    for info in sources:
+        dest = data_dir / info["dir"]
+        if dest.is_dir():
+            continue
+        tar = download_dir / info["tar"]
+        if not tar.is_file():
+            print("downloading", info["url"])
+            urllib.request.urlretrieve(info["url"], tar)
+        with tarfile.open(tar) as fp:
+            fp.extractall(dest, filter="data")
+dl21([sources(synset, readable) for _, synset, readable in balls])
 
 # import nltk; nltk.download()
 ids21 = "https://storage.googleapis.com/bit_models/imagenet21k_wordnet_ids.txt"
@@ -57,9 +58,9 @@ def wn_balls():
         for synset in fp:
             if hyper in wn21(synset).hypernyms():
                 balls21.append(synset.strip())
-    return balls21, [wn21(i) for i in balls21]
+    return balls21, [wn21(i)._name.split('.')[0] for i in balls21]
 
-# balls21 = wn_balls()
+# balls21, readable = wn_balls()
 balls21 = [
         'n02799071', 'n02802426', 'n02839351', 'n02861147', 'n02881546',
         'n02882301', 'n03131967', 'n03134739', 'n03333252', 'n03378765',
@@ -67,3 +68,12 @@ balls21 = [
         'n03742019', 'n03825442', 'n03942813', 'n03978575', 'n03982232',
         'n04023962', 'n04039742', 'n04113316', 'n04118538', 'n04254680',
         'n04256891', 'n04292221', 'n04409515', 'n04540053', 'n04584056']
+readable = [
+        'baseball', 'basketball', 'billiard_ball', 'bocce_ball', 'bowl',
+        'bowling_ball', 'cricket_ball', 'croquet_ball', 'field_hockey_ball',
+        'football', 'golf_ball', 'handball', 'jack', 'lacrosse_ball', 'marble',
+        'medicine_ball', 'ninepin_ball', 'ping-pong_ball', 'polo_ball',
+        'pool_ball', 'punching_bag', 'racquetball', 'roulette_ball',
+        'rugby_ball', 'soccer_ball', 'softball', 'squash_ball', 'tennis_ball',
+        'volleyball', 'wiffle']
+dl21(map(lambda a: sources(*a), zip(balls21, readable)))
