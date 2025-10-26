0da3f05ba7b95cad8673a6c3b76a0dbb3e17552a
Kent Slaney <kent@slaney.org>
2025-10-25 20:00:16 -0700

    Depth Anything V2 Small predictions to tfrecords
    mostly LLM generated

--- ds12.py
+++ ds12.py
@@ -7,6 +7,7 @@ import xml.etree.ElementTree as ET
 local = pathlib.Path(__file__).parents[0]
 sys.path.insert(0, str(local))
 from wn12 import balls
+from oop import Da2
 sys.path.pop(0)
 
 ds12, info12 = tfds.load("imagenet2012", with_info=True)
@@ -62,7 +63,10 @@ def write_example(writer, file):
     writer.write(example.SerializeToString())
 
 all_records = data_base / "bndbox.tfrecord"
-ball_records = data_base / "balls.tfrecord"
+
+dsfs = local / "assets" / "datasets"
+dsfs.mkdir(parents=True, exist_ok=True)
+ball_records = dsfs / "bndbox_balls.tfrecord"
 
 def write_all():
     with tf.io.TFRecordWriter(str(all_records)) as writer:
@@ -128,3 +132,120 @@ def filtered12():
     def _filter(x):
         return tf.math.reduce_any(x['label'] == ids)
     return balls12().filter(_filter)
+
+model = Da2('vits')
+
+balls_depth_records = dsfs / "depth_balls.tfrecord"
+ballnt_depth_records = dsfs / "depth_ballnt.tfrecord"
+
+def write_balls_depth_records():
+    ds = filtered12()
+
+    def _get_depth(image_tensor):
+        # The model expects a numpy array.
+        image_numpy = image_tensor.numpy()
+        depth_numpy = model(image_numpy)
+        return depth_numpy.astype('float32')
+
+    def _process_with_depth(example):
+        depth_tensor = tf.py_function(
+            _get_depth, [example['image']], tf.float32)
+        # The model output shape is not static, so we don't set it here.
+        return {**example, 'depth': depth_tensor}
+
+    ds_with_depth = ds.map(_process_with_depth)
+
+    with tf.io.TFRecordWriter(str(balls_depth_records)) as writer:
+        for example in ds_with_depth:
+            feature = {
+                'file_name': tf.train.Feature(bytes_list=tf.train.BytesList(value=[example['file_name'].numpy()])),
+                'image': tf.train.Feature(bytes_list=tf.train.BytesList(value=[tf.io.serialize_tensor(example['image']).numpy()])),
+                'depth': tf.train.Feature(bytes_list=tf.train.BytesList(value=[tf.io.serialize_tensor(example['depth']).numpy()])),
+                'label': tf.train.Feature(bytes_list=tf.train.BytesList(value=[tf.io.serialize_tensor(example['label']).numpy()])),
+                'bbox': tf.train.Feature(int64_list=tf.train.Int64List(value=tf.reshape(example['bbox'], [-1]).numpy()))
+            }
+            tf_example = tf.train.Example(features=tf.train.Features(feature=feature))
+            writer.write(tf_example.SerializeToString())
+
+def read_balls_depth_records():
+    depth_feature_description = {
+        'file_name': tf.io.FixedLenFeature([], tf.string),
+        'image': tf.io.FixedLenFeature([], tf.string),
+        'depth': tf.io.FixedLenFeature([], tf.string),
+        'label': tf.io.FixedLenFeature([], tf.string),
+        'bbox': tf.io.FixedLenFeature([17 * 4], tf.int64)
+    }
+
+    ds = tf.data.TFRecordDataset(str(balls_depth_records))
+
+    def _parse_depth_function(x):
+        res = tf.io.parse_single_example(x, depth_feature_description)
+        return {
+            'file_name': res['file_name'],
+            'image': tf.io.parse_tensor(res['image'], out_type=tf.uint8),
+            'depth': tf.io.parse_tensor(res['depth'], out_type=tf.float32),
+            'label': tf.io.parse_tensor(res['label'], out_type=tf.int64),
+            'bbox': tf.reshape(tf.cast(res['bbox'], tf.int32), (17, 4))
+        }
+
+    return ds.map(_parse_depth_function)
+
+# >>> sum(1 for _ in read_depth_records())
+# 10382
+
+def read_ballnt_depth_records():
+    ballnt_depth_feature_description = {
+        'file_name': tf.io.FixedLenFeature([], tf.string),
+        'image': tf.io.FixedLenFeature([], tf.string),
+        'depth': tf.io.FixedLenFeature([], tf.string),
+        'label': tf.io.FixedLenFeature([], tf.string),
+    }
+
+    ds = tf.data.TFRecordDataset(str(ballnt_depth_records))
+
+    def _parse_ballnt_depth_function(x):
+        res = tf.io.parse_single_example(x, ballnt_depth_feature_description)
+        return {
+            'file_name': res['file_name'],
+            'image': tf.io.parse_tensor(res['image'], out_type=tf.uint8),
+            'depth': tf.io.parse_tensor(res['depth'], out_type=tf.float32),
+            'label': tf.io.parse_tensor(res['label'], out_type=tf.int64),
+        }
+
+    return ds.map(_parse_ballnt_depth_function)
+
+def write_ballnt_depth_records():
+    """
+    Filters for images not in the 'balls' synsets, takes 10382 of them,
+    calculates their depth maps, and writes them to ballnt_depth.tfrecord.
+    """
+    def _filter_not_balls(x):
+        return tf.math.logical_not(tf.math.reduce_any(x['label'] == ids))
+
+    ds = ds12['train'].filter(_filter_not_balls).take(10382)
+
+    def _get_depth(image_tensor):
+        # The model expects a numpy array.
+        image_numpy = image_tensor.numpy()
+        depth_numpy = model(image_numpy)
+        return depth_numpy.astype('float32')
+
+    def _process_with_depth(example):
+        depth_tensor = tf.py_function(
+            _get_depth, [example['image']], tf.float32)
+        return {**example, 'depth': depth_tensor}
+
+    ds_with_depth = ds.map(_process_with_depth)
+
+    with tf.io.TFRecordWriter(str(ballnt_depth_records)) as writer:
+        for i, example in enumerate(ds_with_depth):
+            if (i + 1) % 1000 == 0:
+                print(f"Writing record {i + 1}/10382 for ballnt_depth.tfrecord")
+            feature = {
+                'file_name': tf.train.Feature(bytes_list=tf.train.BytesList(value=[example['file_name'].numpy()])),
+                'image': tf.train.Feature(bytes_list=tf.train.BytesList(value=[tf.io.serialize_tensor(example['image']).numpy()])),
+                'depth': tf.train.Feature(bytes_list=tf.train.BytesList(value=[tf.io.serialize_tensor(example['depth']).numpy()])),
+                'label': tf.train.Feature(bytes_list=tf.train.BytesList(value=[tf.io.serialize_tensor(example['label']).numpy()])),
+            }
+            tf_example = tf.train.Example(features=tf.train.Features(feature=feature))
+            writer.write(tf_example.SerializeToString())
