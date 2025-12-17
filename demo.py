63a556193fb5c0e7c4c14eafb7f109bcf4d50d4d
Kent Slaney <kent@slaney.org>
2025-12-16 22:25:44 -0800

    delerium encroaching

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 import cv2
 import torch
 import threading
-import queue
+from collections import deque
 import time
 import numpy as np
 from PIL import Image
@@ -13,31 +13,47 @@ from simplified import *
 sys.path.pop(0)
 
 # TODO: pipeline batches up to an acceptable latency
-input_queue = queue.Queue(maxsize=1)
-output_queue = queue.Queue(maxsize=1)
+input_queue = deque(maxlen=1)
+output_queue = deque(maxlen=1)
+daemon_stop = threading.Event()
+
+class Demo(Demo):
+    def uncrop(self, coords):
+        if self.target is None:
+            return coords
+        size = jnp.array(self.spec)
+        unscale = jnp.max(self.shape[::-1] / size)
+        scaled = jnp.int32(unscale * size)
+        origin = (scaled - self.shape[::-1]) // 2
+        return jnp.tile(origin, [1, 2]) + coords * unscale
+
+def demo_model(arr):
+    im = Demo(arr)
+    _, bboxes = im.depth.binned().nominate()
+    return im.uncrop(bboxes)
 
 class PyTorchWorker(threading.Thread):
     def __init__(self, model):
         super().__init__(daemon=True)
         self.model = model
-        self.stop = threading.Event()
 
     def run(self):
+        daemon_stop.clear()
         # shouldn't be any CPU bottlenecks
         torch.set_num_threads(1)
 
-        while not stop_event.is_set():
-            frame = input_queue.get()
-            # no eager io
-            results = self.model(frame.copy())
+        while not daemon_stop.is_set():
+            try:
+                # TODO: is there a good reason deque doesn't have blocking pop?
+                frame = input_queue.pop()
+            except IndexError:
+                time.sleep(0.01)
+                continue
+            frame_rgb = cv2.cvtColor(frame.copy(), cv2.COLOR_BGR2RGB)
+            results = self.model(Image.fromarray(frame_rgb))
 
-            # Clear old result
-            if not output_queue.empty():
-                try:
-                    output_queue.get_nowait()
-                except queue.Empty:
-                    pass
-            output_queue.put(results)
+            print(results)
+            output_queue.append(results)
 
 def pollnt(*titles):
     try:
@@ -48,17 +64,16 @@ def pollnt(*titles):
         return True
     return False
 
-def rect(im, bbox, color=(0, 255, 0), thickness=1, **kw):
-    bbox = bbox.tolist() if hasattr(bbox, 'dtype') else bbox
-    return cv2.rectangle(im, bbox[1::-1], bbox[3:1:-1], color, thickness, **kw)
-
-async def preview_dispatch():
-    pass
+def rect(im, *bboxes, color=(0, 255, 0), thickness=1, **kw):
+    for bbox in bboxes:
+        bbox = bbox.tolist() if hasattr(bbox, 'dtype') else bbox
+        cv2.rectangle(im, bbox[1::-1], bbox[3:1:-1], color, thickness, **kw)
 
 cap = None
+worker = PyTorchWorker(demo_model)
 
 def main(count_bboxes=3, live_bboxes=3):
-    global cap
+    global cap, worker
     if cap is not None:
         clean()
     cap = cv2.VideoCapture(0)
@@ -68,6 +83,7 @@ def main(count_bboxes=3, live_bboxes=3):
         cv2.namedWindow("preview")
         queue.append(titles)
     window("preview")
+    preview_bboxes = False
 
     while queue:
         ret, frame = cap.read()
@@ -75,8 +91,6 @@ def main(count_bboxes=3, live_bboxes=3):
         if not ret:
             print("Can't receive frame (stream end?). Exiting ...")
             break
-
-        cv2.imshow('preview', frame)
 
         spin_key = cv2.waitKey(1) & 0xFF
         if spin_key == ord('q'):
@@ -107,14 +121,29 @@ def main(count_bboxes=3, live_bboxes=3):
                 sys.stdout.flush()
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
-        elif spin_key == ord('f'):
-            pass
+        elif spin_key == ord('g'):
+            if not daemon_stop.is_set():
+                worker.start()
+            # else:
+            #     daemon_stop.set()
+
+        if preview_bboxes:
+            try:
+                bboxes = output_queue.pop()
+                pending, frame = frame, frame.copy()
+                rect(frame, *bboxes)
+            except IndexError:
+                pending = frame
+            input_queue.append(pending)
 
         it = enumerate(queue)
         for level, group in it:
             if pollnt(*group):
                 for sibling in group:
-                    cv2.destroyWindow(sibling)
+                    try:
+                        cv2.destroyWindow(sibling)
+                    except cv2.error:
+                        return True
                 parent = level - 1
                 for level, children in it:
                     for child in children:
@@ -122,10 +151,13 @@ def main(count_bboxes=3, live_bboxes=3):
                 for i in range(level - parent):
                     queue.pop()
 
+        cv2.imshow('preview', frame)
+
     clean()
 
 def clean():
     global cap
+    daemon_stop.set()
     cap.release()
     cap = None
     clear()
