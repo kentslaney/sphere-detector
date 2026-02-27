99f4f1dbcc629cf01c75b2b87658409b0693c212
Kent Slaney <kent@slaney.org>
2026-02-27 10:37:09 -0800

    duplicate export setup

---
+++ src/depth-test/unit.py
@@ -0,0 +1,65 @@
+import jax, json
+import jax.numpy as jnp
+import coremltools as ct
+
+from jax._src.lib.mlir import ir
+from jax._src.interpreters import mlir as jax_mlir
+from jax.export import export
+
+from stablehlo_coreml import DEFAULT_HLO_PIPELINE
+
+from .detect import Config, Raster
+from .utils import dist
+from .mil import convert
+
+target = Config.resolution
+
+@jax.jit
+def jax_density(x):
+    return x
+
+context = jax_mlir.make_ir_context()
+input_shapes = (jnp.zeros((1, 1) + target, dtype=jnp.float16),)
+jax_exported = export(jax_density)(*input_shapes)
+hlo_module = ir.Module.parse(jax_exported.mlir_module(), context=context)
+
+mil_program = convert(hlo_module)
+
+mil_args = mil_program.functions[
+        mil_program.default_function_name].inputs.keys()
+mil_arg0 = next(iter(mil_args))
+
+pipeline = DEFAULT_HLO_PIPELINE
+pipeline.set_options("common::const_elimination", {"skip_const_by_size": "1e2"})
+# pipeline.remove_passes(['common::add_int16_cast'])
+
+import logging
+from coremltools import _logger as logger
+# logger_level = logger.level
+# logger.setLevel(logging.ERROR)
+logging.getLogger("coremltools").disabled = True
+
+cml_model = ct.convert(
+    mil_program,
+    source="milinternal",
+    minimum_deployment_target=ct.target.iOS18,
+    compute_units=ct.ComputeUnit.ALL,
+    # compute_units=ct.ComputeUnit.CPU_ONLY,
+    pass_pipeline=pipeline,
+    inputs=[ct.ImageType(
+        mil_arg0, shape=target, color_layout=ct.colorlayout.GRAYSCALE_FLOAT16)],
+)
+
+# logger.setLevel(logger_level)
+logging.getLogger("coremltools").disabled = False
+
+import numpy as np
+from PIL import Image
+
+from .examples import im4
+
+pil_depth = Image.fromarray(np.array(im4.depth.depth))
+cml_out = cml_model.predict({"_arg0": pil_depth})
+fmt_kw = {"sep": "\n", "end": "\n\n"}
+print("CoreML", cml_out, **fmt_kw)
+print("Jax", im4.depth.depth, **fmt_kw)
