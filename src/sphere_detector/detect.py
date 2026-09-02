4c52f9c924624083bc7fb06750f66f13cd698cdb
Kent Slaney <kent@slaney.org>
2026-09-01 17:17:29 -0700

    config params

--- src/sphere_detector/detect.py
+++ src/sphere_detector/detect.py
@@ -36,7 +36,7 @@ class Config:  # hyperparameters
     phi: any = (1 + math.sqrt(5)) / 2  # metric dimensionality
 
     # AliasedRay
-    alpha: any = 0.0  # standard deviations above mean for ray start depth
+    alpha: any = 0.5  # standard deviations above mean for ray start depth
     # mean height vs center: 2 / 3 * r and standard deviation: sqrt(2) / 6 * r
     beta: any = 3.0  # standard deviations below mean for ray start depth
     gamma: any = 0.2  # interpolation value between median and mean for w
@@ -44,9 +44,8 @@ class Config:  # hyperparameters
     chi: any = 0.5  # standard deviations above initial mean radius to look
 
     # Surface
-    # TODO: tune
     mu: any = 1.0  # edge RMSE coefficient (remember via lower case shape)
-    nu: any = 1.0  # depth slice RMSE coefficient
+    nu: any = 0.3  # depth slice RMSE coefficient
 
     depth_checkpoint = "vits"
 
